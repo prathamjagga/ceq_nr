fe7b8acb19b397a56dbee02c155208b8db00f13b
Pratham Jagga <pratham.jagga@cloudeq.com>
2023-08-07 11:15:52 +0530

    add charts

---
+++ adorable-hammer/nerdlets/home/linechart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { LineChart } from "nr1";
+
+export default class Linechart extends React.Component {
+  render() {
+    const linechart1 = {
+      metadata: {
+        id: "line-chart-1",
+        name: "Version A",
+        viz: "main",
+        color: "red",
+      },
+      data: [
+        { x: 0, y: 0 },
+        { x: 10, y: 20 },
+        { x: 30, y: 40 },
+      ],
+    };
+    //  return <LineChart data={[linechart1]} fullWidth />;
+    return (
+      <LineChart
+        accountId={4043634}
+        query="SELECT average(host.cpuPercent) AS 'CPU used %' FROM Metric WHERE `entityGuid` = 'NDA1NTg4NnxJTkZSQXxOQXw5MTc5MzkxNjUwMjU3NDU3MzUw' TIMESERIES auto"
+        fullWidth
+      />
+    );
+  }
+}
---
+++ adorable-hammer/nerdlets/home/piechart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { PieChart } from "nr1";
+
+export default class Piechart extends React.Component {
+  render() {
+    const piechart1 = [
+      {
+        metadata: {
+          id: "series-1",
+          name: "Serie 1",
+          color: "#a35ebf",
+          viz: "main",
+          units_data: {
+            y: "BYTES",
+          },
+        },
+        data: [{ y: 128 }],
+      },
+      {
+        metadata: {
+          id: "series-2",
+          name: "Serie 2",
+          color: "#85c956",
+          viz: "main",
+          units_data: {
+            y: "BYTES",
+          },
+        },
+        data: [{ y: 256 }],
+      },
+    ];
+    // return <PieChart data={piechart1} fullWidth />;
+    return (
+      <PieChart
+        accountId={4043634}
+        query="SELECT latest(host.process.cpuPercent) as 'CPU %', latest(host.process.threadCount) as 'Threads' FROM Metric FACET processId, processDisplayName WHERE `entityGuid` = 'NDA1NTg4NnxJTkZSQXxOQXw5MTc5MzkxNjUwMjU3NDU3MzUw' ORDER BY cpuPercent asc LIMIT 100"
+        fullWidth
+      />
+    );
+  }
+}
